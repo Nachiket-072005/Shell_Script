d612a7cb0500ca52be8842205f6e2519bea7eb2a
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-07-11 09:02:17 +0530

    ShellScript Variables

---
+++ ShellScript/Variables/hellothere.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+FIRST_NAME=Nachiket
+LAST_NAME=Prajapati
+echo Hello $FIRST_NAME $LAST_NAME
---
+++ ShellScript/Variables/interective.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo What is your first name?
+read FIRST_NAME
+echo What is your last name?
+read LAST_NAME
+
+echo Hello $FIRST_NAME $LAST_NAME
